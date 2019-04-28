5fc4bc6bec600fa7261165e199d9142b3f8a90d3
L <manguluka@gmx.com>
2019-04-28 11:08:26 -0700

    enable commonjs rollup output

--- rollup.config.js
+++ rollup.config.js
@@ -12,11 +12,11 @@ import pkg from './package.json'
 export default {
   input: 'src/index.js',
   output: [
-    // {
-    //   file: pkg.main,
-    //   format: 'cjs',
-    //   sourcemap: true
-    // },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
     {
       file: pkg.module,
       format: 'es',
